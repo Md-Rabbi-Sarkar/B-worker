c66703ec8be873725121d94a32333b07aaf40041
Md-Rabbi-Sarkar <noreply@github.com>
2025-01-22 13:10:11 +0600

    making pagination systme in mysubmission page for worker

--- src/Pages/Dashboard/Worker/MySubmission.jsx
+++ src/Pages/Dashboard/Worker/MySubmission.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../../../Providers/AuthProvider'
 import useAxiosPublic from '../../../Hooks/useAxiosPublic'
 import useAxiosSecure from '../../../Hooks/useAxiosSecure'
@@ -7,14 +7,41 @@ import { useQuery } from '@tanstack/react-query'
 export default function MySubmission() {
     const { user } = useContext(AuthContext)
     const axiosSecure = useAxiosSecure()
+    const [itemsPerPage, setItemsPerpage] =useState(5)
+    const [currentPage, setCurrentPage] = useState(0)
     const {data:submissionTask =[]}=useQuery({
-        queryKey:['submitTask',user?.email],
-        queryFn: async()=>{
-            const res = await axiosSecure.get(`/submitTask/${user.email}`)
-           return res.data
-        }
-    })
+      queryKey:['submitTask',user?.email,currentPage,itemsPerPage],
+      queryFn: async()=>{
+          const res = await axiosSecure.get(`/submitTask?email=${user.email}&page=${currentPage}&size=${itemsPerPage}`)
+         return res.data
+      }
+  })
     
+    const totaldata = submissionTask.length
+    // console.log(totaldata)
+    // const itemPerPage = 1
+    const numberOfPage = Math.ceil(totaldata/itemsPerPage)
+    // console.log(numberOfPage)
+    const pages = [ ...Array(numberOfPage).keys()]
+    // console.log(pages)
+    const handleItemsPerPage =e =>{
+      // console.log(e.target.value)
+      const val = parseInt(e.target.value)
+      console.log(val)
+      setItemsPerpage(val)
+      setCurrentPage(0)
+    }
+    const handlePre =() =>{
+      if(currentPage >0 ){
+        setCurrentPage(currentPage-1)
+      }
+    }
+    const handleNext =()=>{
+      if(currentPage<pages.length -1){
+        setCurrentPage(currentPage + 1)
+      }
+    }
+
     return (
         <div className="overflow-x-auto">
   <table className="table">
@@ -28,7 +55,7 @@ export default function MySubmission() {
       </tr>
     </thead>
     <tbody>
-      {submissionTask.map((task,index)=><tr>
+      {submissionTask.map((task,index)=><tr key={index} >
         <th>{index+1}</th>
         <td>{task.taskTitle}</td>
         <td>{task.submissionDate}</td>
@@ -38,6 +65,18 @@ export default function MySubmission() {
       
     </tbody>
   </table>
+  <div>
+  <button onClick={handlePre} className="join-item btn btn-outline">Previous</button>
+    {pages.map(page=>  <button className={currentPage ===page ?'join-item btn btn-active' :'join-item btn'} key={page} onClick={()=>setCurrentPage(page)}>{page}</button>)}
+    <button onClick={handleNext} className="join-item btn btn-outline">Next</button>
+        <select defaultValue={itemsPerPage} onChange={handleItemsPerPage} name="" id="">
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="40">40</option>
+        </select>
+        
+  </div>
 </div>
     )
 }
